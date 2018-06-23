12b718357428fc57be8cae6b42a3bbeb8471001e
Alexander Mitic <miticalexander@yahoo.com>
2018-06-23 22:53:06 +0200

    Added specific transitions class and infinite rotation
    - Added a class for specific type of transitions
    ** added Infinite rotation (period, direction, axis and group needed to make one)

--- RoundaboutInAYard/src/objects/Turntable.java
+++ RoundaboutInAYard/src/objects/Turntable.java
@@ -9,13 +9,23 @@ import javafx.scene.shape.Cylinder;
  * @author AM
  */
 public class Turntable extends Cylinder{
-    private double rotationPeriod = 10;
-    private double rotationDirection = 1;
-    
     private PhongMaterial material = new PhongMaterial();
     private RotateTransition rotation = new RotateTransition();
+
+    public RotateTransition getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(RotateTransition rotation) {
+        this.rotation = rotation;
+    }
     
-    public Turntable(double radius, double height){
+    public void setRotation(double rotationPeriod, double rotationDirection) {
+        this.rotation = rotation;
+    }
+    
+    public Turntable(double radius, double height, 
+            double rotationPeriod, double rotationDirection){
         Cylinder p = new Cylinder(radius, height, 100);
     }
 }
