3479e9d83fbfaf50c10f00689301b45e68aac0a8
Alexander Mitic <miticalexander@yahoo.com>
2018-06-23 22:53:29 +0200

    Revert "Added specific transitions class and infinite rotation"
    This reverts commit 12b718357428fc57be8cae6b42a3bbeb8471001e.

--- RoundaboutInAYard/src/objects/Turntable.java
+++ RoundaboutInAYard/src/objects/Turntable.java
@@ -9,23 +9,13 @@ import javafx.scene.shape.Cylinder;
  * @author AM
  */
 public class Turntable extends Cylinder{
+    private double rotationPeriod = 10;
+    private double rotationDirection = 1;
+    
     private PhongMaterial material = new PhongMaterial();
     private RotateTransition rotation = new RotateTransition();
-
-    public RotateTransition getRotation() {
-        return rotation;
-    }
-
-    public void setRotation(RotateTransition rotation) {
-        this.rotation = rotation;
-    }
     
-    public void setRotation(double rotationPeriod, double rotationDirection) {
-        this.rotation = rotation;
-    }
-    
-    public Turntable(double radius, double height, 
-            double rotationPeriod, double rotationDirection){
+    public Turntable(double radius, double height){
         Cylinder p = new Cylinder(radius, height, 100);
     }
 }
