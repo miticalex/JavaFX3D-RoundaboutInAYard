13eb35d8c590bc6933c966052ae62e984cf7bf3f
Alexander Mitic <miticalexander@yahoo.com>
2018-06-25 02:49:19 +0200

    Added Rotating Turntable and 4 Rotating Chairs on it

--- RoundaboutInAYard/src/roundaboutinayard/RoundaboutInAYard.java
+++ RoundaboutInAYard/src/roundaboutinayard/RoundaboutInAYard.java
@@ -5,14 +5,11 @@
  */
 package roundaboutinayard;
 
-import javafx.animation.RotateTransition;
 import javafx.application.Application;
-import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.transform.Rotate;
@@ -52,47 +49,40 @@ public class RoundaboutInAYard extends Application {
         return camera;
     }
     
-//    private Turntable makeTurntableWithChairs(int numChairs) {
-//        Turntable turntable = new Turntable(TURNTABLE_R, TURNTABLE_H);
-//        turntable.setInfiniteRotation(TURNTABLE_ROTATION_PERIOD, SpecificTransitions.POSITIVE_ROTATION);
-//        
-//        Chair[] chairs = new Chair[numChairs];
-//        
-//        for (int i = 0; i < numChairs; i++) {
-//            chairs[i] = new Chair();
-//            turntable.getChildren().add(chairs[i]);
-//            
-//            chairs[i].setTranslateY(TURNTABLE_R/2 + Chair.CHAIR_STAND_HEIGHT/2);
-//            chairs[i].setRotationAxis(Rotate.Y_AXIS);
-//            chairs[i].setRotate(i*SpecificTransitions.FULL_ROTATION / numChairs);
-//            chairs[i].setTranslateX(0.75 * TURNTABLE_R);
-//            
-//            chairs[i].setInfiniteRotation(Chair.CHAIR_STAND_ROTATION_PERIOD, SpecificTransitions.CLOCKWISE_ROTATION);
-//        }
-//        
-////        Image image = new Image("metal.jpg");
-////        
-////        PhongMaterial material = new PhongMaterial();
-////        material.setDiffuseMap(image);
-////        turntable.setMaterial(material);
-//        
-//        root.getChildren().add(turntable);
-//        
-//        return turntable;
-//    }
-    
-    private void addChair(){
-        Chair chair = new Chair();
-        chair.setInfiniteRotation(Chair.CHAIR_STAND_ROTATION_PERIOD, SpecificTransitions.POSITIVE_ROTATION);
-        root.getChildren().add(chair);
+    private Turntable makeTurntableWithChairs(int numChairs) {
+        Turntable turntable = new Turntable(TURNTABLE_R, TURNTABLE_H);
+        turntable.setMaterial(new PhongMaterial(TURNTABLE_COLOR));
+        turntable.setInfiniteRotation(TURNTABLE_ROTATION_PERIOD, SpecificTransitions.COUNTERCLOCKWISE_ROTATION);
+        
+        Chair[] chairs = new Chair[numChairs];
+        
+        for (int i = 0; i < numChairs; i++) {
+            chairs[i] = new Chair();
+            
+            chairs[i].setTranslateY(-(TURNTABLE_H/2 + Chair.CHAIR_STAND_HEIGHT/2));
+            chairs[i].setTranslateX(0.75 * TURNTABLE_R * Math.cos(i * 2*Math.PI/numChairs));
+            chairs[i].setTranslateZ(0.75 * TURNTABLE_R * Math.sin(i * 2*Math.PI/numChairs));
+            
+            chairs[i].setRotationAxis(Rotate.Y_AXIS);
+            chairs[i].setRotate(-(i*SpecificTransitions.FULL_ROTATION / numChairs - 90));
+            
+//            Translate translate1 = new Translate(0, -(TURNTABLE_H/2 + Chair.CHAIR_STAND_HEIGHT/2), 0);
+//            Rotate rotate2 = new Rotate(i*SpecificTransitions.FULL_ROTATION / numChairs, Rotate.Y_AXIS);
+//            Translate translate3 = new Translate(0, 0, 0.75 * TURNTABLE_R);
+//            chairs[i].getTransforms().setAll(translate1, rotate2, translate3);
+
+            chairs[i].setInfiniteRotation(Chair.CHAIR_STAND_ROTATION_PERIOD, SpecificTransitions.CLOCKWISE_ROTATION);
+        }
+        turntable.getChildren().addAll(chairs);
+        
+        return turntable;
     }
     
     @Override
     public void start(Stage primaryStage) {
         perspectiveCamera = makePerspectiveCamera();
-//        turntable = makeTurntableWithChairs(4);
-        addChair();
-
+        turntable = makeTurntableWithChairs(4);
+        root.getChildren().add(turntable);
         
         Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, true, SceneAntialiasing.BALANCED);
         scene.setCamera(perspectiveCamera);
